529f7429debfe310448f0bc7877e92589fefcae6
Rafael Roman <rafael.roman@gft.com>
2016-05-25 16:25:20 +0200

    adding token name to tokens.

--- src/main/java/org/appverse/builder/config/OAuth2ServerConfiguration.java
+++ src/main/java/org/appverse/builder/config/OAuth2ServerConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -16,11 +18,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultAuthenticationKeyGenerator;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.appverse.builder.web.rest.AuthTokenResource.TOKEN_NAME;
 
 @Configuration
 public class OAuth2ServerConfiguration {
@@ -92,8 +102,51 @@ public class OAuth2ServerConfiguration {
 
         @Bean
         public TokenStore tokenStore() {
-            return new JdbcTokenStore(dataSource);
+
+            JdbcTokenStore jdbcTokenStore = new JdbcTokenStore(dataSource) {
+                @Override
+                public OAuth2AccessToken getAccessToken(OAuth2Authentication authentication) {
+                    OAuth2AccessToken existingAccessToken = super.getAccessToken(authentication);
+                    //for Named Tokens we should never return an existing token that has a different name than requested
+                    String tokenName = authentication.getOAuth2Request().getRequestParameters().get(TOKEN_NAME);
+                    if (tokenName != null && existingAccessToken != null){
+                        if (!tokenName.equals(existingAccessToken.getAdditionalInformation().get(TOKEN_NAME))) {
+                            return null;
+                        }
+                    }
+                    return existingAccessToken;
+                }
+            };
+
+            jdbcTokenStore.setAuthenticationKeyGenerator(new DefaultAuthenticationKeyGenerator() {
+                @Override
+                public String extractKey(OAuth2Authentication authentication) {
+                    String key = super.extractKey(authentication);
+                    return Optional.ofNullable(authentication.getOAuth2Request().getRequestParameters().get(TOKEN_NAME))
+                        .map(s -> key + "-" + s)
+                        .orElse(key);
+                }
+            });
+            return jdbcTokenStore;
         }
+
+        @Bean
+        public TokenEnhancer tokenEnhancer() {
+            return (accessToken, authentication) -> {
+                Optional.ofNullable(authentication.getOAuth2Request())
+                    .ifPresent(oAuth2Request -> Optional.ofNullable(oAuth2Request.getRequestParameters())
+                        .ifPresent(params -> Optional.ofNullable(params.get(TOKEN_NAME))
+                            .ifPresent(name -> {
+                                if (accessToken instanceof DefaultOAuth2AccessToken) {
+                                    Map<String, Object> additionalInformation = new HashMap<>(accessToken.getAdditionalInformation());
+                                    additionalInformation.put(TOKEN_NAME, name);
+                                    ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
+                                }
+                            })));
+                return accessToken;
+            };
+        }
+
 
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints)
@@ -101,6 +154,7 @@ public class OAuth2ServerConfiguration {
 
             endpoints
                 .tokenStore(tokenStore())
+                .tokenEnhancer(tokenEnhancer())
                 .authenticationManager(authenticationManager);
         }
 
